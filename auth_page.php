4938f90ce817cf56a992af5bea695140cc473891
Said <saidabdan12@gmail.com>
2026-03-08 22:33:23 +0700

    tes path

--- auth_page.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
-    header("Location: index.php");
-    exit;
-}
--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'db_host' => 'mysql',
-    'db_user' => 'root',
-    'db_pass' => 'Password22_',
-    'db_name' => 'project'
-];
