788bf21a08b9eee0df9b7128ea3f42c60f706b67
Said Abdan Syakur <saidabdan12@gmail.com>
2026-03-17 03:00:41 +0700

    update php dockerfile pake healthcheck

--- docker/Dockerfile.php
+++ docker/Dockerfile.php
@@ -8,6 +8,9 @@ COPY src/ /var/www/html
 
 RUN chown -R www-data:www-data /var/www/html
 
+HEALTHCHECK --interval=30s --timeout=5s --retries=3 \
+ CMD php-fpm -t || exit 1
+
 EXPOSE 9000
 
 CMD ["php-fpm"]
